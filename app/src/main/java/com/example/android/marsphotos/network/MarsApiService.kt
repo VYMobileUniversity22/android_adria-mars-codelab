93392bf5647e2fd0b0491b82f27f66f6a06b5d8c
adria.fernandez <adria.fernandez@col.vueling.com>
2022-11-16 10:38:44 +0100

    [feat] getmarsphoto function

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -2,6 +2,7 @@ package com.example.android.marsphotos.network
 
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com" //creamos constante con la URL de base del servicio web que usaremos
@@ -12,3 +13,16 @@ private val retrofit = Retrofit.Builder()                           //creamos ob
     .build()
 
 
+interface MarsApiService {
+    @GET("photos") //agrega el extremo photos al final del url, como si pusieramos /photos en el navegador
+    fun getPhotos() : String
+}
+
+//objeto publico para inicializar el servicio retrofit
+object MarsApi {
+
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+
+}
